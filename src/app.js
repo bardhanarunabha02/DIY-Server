e558cbfdbe052a7b0bd7f064a86301ca9c840a14
TestUser <test@test.com>
2018-09-23 15:02:34 +0530

    commenting mongoDB reference

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const MongoClient = require('mongodb').MongoClient
+//const MongoClient = require('mongodb').MongoClient
 
 const app = express();
 app.use(morgan('combined'));
